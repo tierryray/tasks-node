589d66ee9b661e7a693c4a51951de854b3ed28db
Tierry Ray <tierry.ray@gmail.com>
2019-06-26 14:37:56 -0300

    Desafio Concluído

--- index.js
+++ index.js
@@ -3,55 +3,79 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
-const projects = [
-  {
-    id: "1",
-    title: "Novo Teste",
-    tasks: ["Nova Tarefa", "Nova Tarefa 2"]
-  }
-];
+const projects = [];
 
+let requests = 0;
+
+server.use((req, res, next) => {
+  requests++;
+  next();
+  console.log(`Number of Requests So Far: ${requests}`);
+});
+
+function checkIfProjectExists(req, res, next) {
+  const project = projects.find(item => item.id === req.params.id);
+
+  if (!project) {
+    res.status(400).json({ error: "Project does not exist" });
+  }
+
+  return next();
+}
+
+//  Lista todos os projetos
 server.get("/projects", (req, res) => {
   return res.json(projects);
 });
 
+//  Adiciona projeto
 server.post("/projects", (req, res) => {
   const { id, title } = req.body;
 
-  projects.push({
+  const project = {
     id,
     title,
     tasks: []
-  });
+  };
 
-  res.send();
+  projects.push(project);
+
+  res.json(project);
 });
 
-server.post("/projects/:id/tasks", (req, res) => {
+//  Adiciona tarefa ao projeto
+server.post("/projects/:id/tasks", checkIfProjectExists, (req, res) => {
   const { title } = req.body;
-  const project = projects[req.params.id];
+  const { id } = req.params;
+
+  const project = projects.find(item => item.id === id);
 
   project.tasks.push(title);
 
   res.json(project);
 });
 
-server.put("/projects/:id", (req, res) => {
+//  Altera título do projeto
+server.put("/projects/:id", checkIfProjectExists, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
+
+  const project = projects.find(item => item.id === id);
+
+  project.title = title;
+
+  return res.json(project);
 });
 
-server.delete("/projects/:id", (req, res) => {
+//  Deleta projeto
+server.delete("/projects/:id", checkIfProjectExists, (req, res) => {
   const { id } = req.params;
 
-  const projectToRemove = projects.find(id => id === id);
+  projects.map((value, index) =>
+    value.id === id ? projects.splice(index, 1) : false
+  );
 
-  projects.delete(projectToRemove);
-
-  // projects.splice(id, 1);
   return res.send();
 });
 
 server.listen(3000);
-
-//  MUDAR FORMA COMO BUSCA POR ID
